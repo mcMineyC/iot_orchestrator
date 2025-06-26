e6418570528bda5d5b8a5ffa01118b002e7c1442
mcMineyC <jedi.master@falconhosting.net>
2025-06-25 17:14:04 -0700

    Start writing kasa integration

---
+++ kasa.js
@@ -0,0 +1,116 @@
+const mqtt = require("mqtt");
+const { Client } = require("tplink-smarthome-api");
+const kasa = new Client();
+const fs = require("fs");
+const client = mqtt.connect("mqtt://127.0.0.1:1883");
+config = JSON.parse(process.argv[2]);
+definition = JSON.parse(fs.readFileSync("config.json", "utf8"));
+definition = definition.knownIntegrations[config.integrationName];
+const clientId = config.id;
+
+client.on("connect", () => {
+  console.log("Connected to broker");
+  definition.schema.forEach((schema) => {
+    if (schema.type == "data" && schema.fetchable) {
+      client.subscribe(`/${config.id}/getdata${schema.path}`, async (err) => {
+        if (!err && err != null) {
+          console.log(
+            "Failed to subscribe to",
+            `/${config.id}/getdata${schema.path}`,
+            ":",
+            err,
+          );
+        }
+      });
+    } else if (schema.type == "command") {
+      client.subscribe(`/${config.id}${schema.path}`, async (err) => {
+        if (!err && err != null) {
+          console.log(
+            "Failed to subscribe to",
+            `/${config.id}${schema.path}`,
+            ":",
+            err,
+          );
+        }
+      });
+    }
+  });
+});
+
+client.on("message", async (topic, message) => {
+  // message is Buffer
+  message = message.toString();
+  console.log(message);
+  try {
+    message = JSON.parse(message);
+  } catch (e) {
+    console.log(
+      "Failed to parse message\n\tTopic:",
+      topic,
+      "\n\tMessage:",
+      message,
+    );
+  } // give a notice of what and where it came from for debugging
+  var path = topic.split("/").slice(2).join("/"); // chop off id since it's not relevant
+  console.log("Received message on path:", path);
+  if (path.split("/")[0] == "getdata") {
+    // eg /$id/getdata/<data_path>
+    console.log("Received request for data from ", path.split("/").slice(1));
+    var data = getData(path.split("/").slice(1).join("/"));
+    if (data == null) {
+      console.log("Data path not found: ", path.split("/").slice(1).join("/"));
+      return;
+    }
+    client.publish(`/${clientId}/${path.split("/")[1]}`, JSON.stringify(data));
+    return;
+  }
+  const handler = commandHandlers["/" + path];
+  if (handler) {
+    const result = await handler(topic, message);
+    client.publish(`/${clientId}${result.path}`, result.data);
+  } else {
+    console.log("Unknown command:", path);
+  }
+});
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//////////////////////////
+///  data fetch logic  ///
+/////////////////////////
+
+function getData(path) {
+  console.log("getData called with path:", "/" + path);
+  switch ("/" + path) {
+    case "/name":
+      return config.config.name;
+    case "/names":
+      return ["Bob", "Frank", "Alice", "Charlie", "David", "Eve"];
+    default:
+      return null;
+  }
+}
+
+//////////////////////////
+/// command definition ///
+/////////////////////////
+
+const commandHandlers = {
+  // Each command handler should be defined here
+  // It takes the topic and message as parameters
+  // Every handler should return an object of the form
+  // { path: string, data: any }
+  "/greet": (topic, message) => {
+    console.log("Hello!");
+    return {
+      path: `/greeting`,
+      data: `Hello, ${message.name}!`,
+    };
+  },
+};
