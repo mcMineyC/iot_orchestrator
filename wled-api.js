029d8f6a00638b898cec275246feefadd9dc4805
mcMineyC <jedi.master@falconhosting.net>
2025-08-02 12:54:01 -0700

    wled module works

--- wled-api.js
+++ wled-api.js
@@ -1,5 +1,6 @@
 import WebSocket from "ws";
 import { EventEmitter } from "node:events";
+import fs from "fs"
 
 export const compare = (a, b) =>
   typeof a === "object" || typeof b === "object"
@@ -12,14 +13,23 @@ export const mapNumRange = (num, inMin, inMax, outMin, outMax) =>
 export class WledApi extends EventEmitter {
   #power = null;
   #brightness = null;
+  #presets = null;
   #preset = null;
   #segments = [];
   #state = null;
   constructor(host) {
     super();
-    this.inited = {}
-    var prom = new Promise((resolve, reject) => {this.inited.resolve = resolve; this.inited.reject = reject})
-    this.inited.promise = prom // The promise needs to be set up, then resolved in another setter
+
+    // Shenanaginry to make sure segments and presets are fetched
+    this.inited = {
+      presets: {},
+      segments: {}
+    }
+    var prom = new Promise((resolve, reject) => {this.inited.segments.resolve = resolve; this.inited.segments.reject = reject})
+    this.inited.segments.promise = prom // The promise needs to be set up, then resolved in another setter
+    this.inited.presets.promise = this.fetchPresets(host);
+
+    // Connect to WLED websocket
     this.ws = new WebSocket(`ws://${host}/ws`);
     this.ws.on("open", function open() {
       console.log("[[WLED]]: Connected to " + host);
@@ -28,6 +38,7 @@ export class WledApi extends EventEmitter {
       console.log("New message!!!");
       try {
         var msg = JSON.parse(data);
+        fs.writeFileSync("./wled-message.json", JSON.stringify(msg,null,2))
         if (typeof msg.success !== "undefined" && msg.success === true)
           console.log("Command completed successfully");
         else if (typeof msg.success !== "undefined" && msg.success === false)
@@ -42,7 +53,47 @@ export class WledApi extends EventEmitter {
     });
   }
   init(){
-    return this.inited.promise
+    return Promise.all([this.inited.segments.promise, this.inited.presets.promise])
+  }
+  async fetchPresets(host) {
+    console.log("Fetching presets from instance");
+    const response = await fetch(`http://${host}/presets.json`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const presets = await response.json();
+    var presetList = [];
+    Object.values(presets).forEach((preset, index) => {
+      presetList.push({
+        brightness: preset.bri,
+        id: index,
+        name: preset.n || `Preset ${index + 1}`,
+      });
+    });
+    console.log("Fetched presets:", presetList.length);
+    this.#_presets = presetList
+    return presetList;
+  }
+
+  set #_power(on){
+    if (on !== this.#power)
+      this.emit("power", on);
+    this.#power = on;
+  }
+  set #_brightness(bri){
+    if(bri !== this.#brightness)
+      this.emit("brightness", bri);
+    this.#brightness = bri;
+  }
+  set #_presets(psl){
+    if(isDiff(psl, this.#presets))
+      this.emit("presets", psl)
+    this.#presets = psl
+  }
+  set #_preset(ps){
+    if(ps !== this.#preset)
+      this.emit("preset", ps)
+    this.#preset = ps
   }
 
   #updateSegments(segs) {
@@ -53,41 +104,40 @@ export class WledApi extends EventEmitter {
         this.#segments[index].state = seg;
       }
     });
-    this.inited.resolve()
+    this.inited.segments.resolve()
   }
 
   set state(s) {
     if (isDiff(this.#state, s)) {
       console.log("State updated")
-      this.#power = s.on;
-      this.#brightness = s.bri;
-      this.#preset = s.ps;
+      this.#_power = s.on
+      this.#_brightness = s.bri
+      this.#_preset = s.ps
       this.#updateSegments(s.seg);
+      this.#state = s;
       this.emit("state", s);
-      console.log("Emitted???")
     }
-    this.#state = s;
   }
   get state() {
     return this.#state;
   }
 
   set power(on){
-
+    this.sendMessage({on})
   }
   get power(){
     return this.#power
   }
 
   set brightness(bri){
-
+    this.sendMessage({bri})
   }
   get brightness(){
     return this.#brightness
   }
 
   set preset(ps){
-    
+    this.sendMessage({ps})
   }
   get preset(){
     return this.#preset
@@ -108,6 +158,8 @@ export class WledSegment extends EventEmitter {
   #power = null;
   #brightness = null;
   #temperature = null;
+  #color = null;
+  #effect = null;
   #currentState = null;
 
   constructor(ws, state) {
@@ -122,8 +174,10 @@ export class WledSegment extends EventEmitter {
   }
 
   set state(state) {
-    this.#id = state.id;
+    this.#id = state.id
     this.#name = state.n;
+
+    // console.log("Segment %d state", this.#id, state)
 
 
     if (state.on !== this.#power)
@@ -134,12 +188,22 @@ export class WledSegment extends EventEmitter {
       this.emit("brightness", state.bri);
     this.#brightness = state.bri;
 
-    if (isDiff(state.cct, this.#temperature))
-      this.emit("temperature", state.cct)
-    this.#temperature = state.cct;
+    // var newTemp = colorConv.rgb2colorTemperature(state.col[0])
+    var col = state.col[0]
+    var newTemp = Math.round(rgbToKelvin({r: col[0], g: col[1], b: col[2]}))
+    if (newTemp !== this.#temperature)
+      this.emit("temperature", newTemp)
+    this.#temperature = newTemp;
+
+    if(state.col[0] != this.#color)
+      this.emit("color", state.col[0])
+    this.#color = state.col[0]
+
+    if(state.fx !== this.#effect)
+      this.emit("effect", state.fx)
+    this.#effect = state.fx
 
     if (this.#currentState === null || isDiff(state, this.#currentState)) {
-      console.log("Segment", this.#id, "state updated")
       this.#currentState = state;
       this.emit("state", state);
     }
@@ -176,15 +240,78 @@ export class WledSegment extends EventEmitter {
   }
 
   set temperature(cct) {
+    // console.log("Asking for temperature %d (cct %d\%)", cctAsK, cct)
     if (compare(cct, this.#temperature)) return;
-    this.sendMessage(cct);
+    var rgbVal = kelvinToRgb(cct)
+    this.sendMessage({fx:0, col: [[rgbVal.r, rgbVal.g, rgbVal.b]]});
     this.emit("temperature", cct);
+    this.#temperature = cct
   }
   get temperature() {
-    return mapNumRange(this.#temperature, 0, 255, 2700, 6500);
+    return this.#temperature
+    // return mapNumRange(this.#temperature, 0, 255, 2700, 6500);
+  }
+
+  set color(col) {
+    if(typeof col.length === "undefined")
+      throw "Color must be array in form [r, g, b]"
+    if(compare(col, this.#color)) return;
+    this.sendMessage({fx: 0, col: [col]})
+    this.emit("color", col)
+  }
+  get color(){
+    return this.#color
+  }
+
+  set effect(fx) {
+    if(compare(fx, this.#effect)) return;
+    this.sendMessage({fx})
+    this.emit("effect", fx)
+  }
+  get effect(){
+    return this.#effect
   }
 
   sendMessage(state) {
     this.ws.send(JSON.stringify({ seg: [{ ...state, id: this.#id }] }));
   }
 }
+
+const clamp = (val, min, max) => Math.min(Math.max(val, min), max);
+function kelvinToRgb(kelvin) {
+    const temp = kelvin / 100;
+    let r, g, b;
+    if (temp < 66) {
+      r = 255
+      g = -155.25485562709179 - 0.44596950469579133 * (g = temp-2) + 104.49216199393888 * Math.log(g)
+      b = temp < 20 ? 0 : -254.76935184120902 + 0.8274096064007395 * (b = temp-10) + 115.67994401066147 * Math.log(b)
+    } else {
+      r = 351.97690566805693 + 0.114206453784165 * (r = temp-55) - 40.25366309332127 * Math.log(r)
+      g = 325.4494125711974 + 0.07943456536662342 * (g = temp-50) - 28.0852963507957 * Math.log(g)
+      b = 255
+    }
+    return {
+      r: clamp(Math.floor(r), 0, 255),
+      g: clamp(Math.floor(g), 0, 255),
+      b: clamp(Math.floor(b), 0, 255)
+    };
+  }
+
+function rgbToKelvin(rgb) {
+    const { r, g, b } = rgb;
+    const eps = 0.4;
+    let minTemp = 2000;
+    let maxTemp = 40000;
+    let temp;
+    while (maxTemp - minTemp > eps) {
+      temp = (maxTemp + minTemp) * 0.5;
+      const rgb = kelvinToRgb(temp);
+      if ((rgb.b / rgb.r) >= (b / r)) {
+        maxTemp = temp;
+      } else {
+        minTemp = temp;
+      }
+    }
+    return temp;
+  }
+
--- wled-test.js
+++ wled-test.js
@@ -1,10 +1,26 @@
+import process from 'process';
+process.stdin.resume(); // Keeps process alive
+
 import {WledApi} from "./wled-api.js"
+
+const TEMP_MIN = 2000;
+const TEMP_MAX = 7000;
+
 const wled = new WledApi("192.168.30.30")
 await wled.init()
-console.log("WLED inited")
 wled.on("state", (state) => {
   console.log("Current state is %s, preset %s", state.on ? "ON" : "OFF", state.ps)
 })
-console.log("There are", wled.segmentslength, "segments")
+console.log("There are", wled.segments.length, "segments")
 wled.segments.forEach((seg) => seg.on("power", (power) => {
   console.log("Segment",seg.id,"("+seg.name+")", "is", power ? "on" : "off")}))
+wled.on("brightness", (bri) => console.log("New brightness is %d", bri))
+wled.segments[3].on("temperature", (t) => {
+  console.log("Desk light changed to %d", t)
+})
+wled.segments[4].on("color", (c) => {console.log("Deks light color changed", c)})
+wled.segments[3].temperature = 5000 
+wled.segments[2].color = [0,255,255]
+wled.segments[5].brightness = 200
+wled.segments[1].power = false
+wled.segments[0].power = true
