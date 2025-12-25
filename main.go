5ac274edf64f9b130ce067f54807025aa714e705
mcMineyC <jedi.master@falconhosting.net>
2025-12-25 15:33:53 -0800

    Revert 7b71f2 (automatically unmarshal nested JSON) due to breakage with non-JSON data

--- main.go
+++ main.go
@@ -271,16 +271,7 @@ func startIntegrations(config *MainConfig, client *mqtt.Client) {
 					if mainState.IntegrationStates[integration.Id] == nil {
 						mainState.IntegrationStates[integration.Id] = make(map[string]any)
 					}
-					var data map[string]any;
-					var saving any;
-					if s := json.Unmarshal([]byte(payload), &data); s != nil {
-						log.Printf("Was able to unmarshal (aka is JSON)")
-						saving = data
-					}else{
-						log.Printf("Was NOT able to unmarshal (aka NOT JSON)")
-						saving = payload
-					}
-					mainState.IntegrationStates[integration.Id][schema.Path] = saving
+					mainState.IntegrationStates[integration.Id][schema.Path] = payload
 					log.Printf("Updated state for %s: %s = %s", integration.Id, schema.Path, payload)
 					// Publish the updated state
 				})
